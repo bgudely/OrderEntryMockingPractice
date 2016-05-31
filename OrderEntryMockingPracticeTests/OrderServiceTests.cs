abf6900e2681490d2e1dca29485f0d04bc21baaa
Brian Gudely <bgudely@paraport.com>
2016-05-31 08:50:29 -0700

    Adding the first test

--- OrderEntryMockingPracticeTests/OrderServiceTests.cs
+++ OrderEntryMockingPracticeTests/OrderServiceTests.cs
@@ -1,6 +1,32 @@
-﻿namespace OrderEntryMockingPracticeTests
+﻿using NUnit.Framework;
+using OrderEntryMockingPractice.Models;
+using OrderEntryMockingPractice.Services;
+
+namespace OrderEntryMockingPracticeTests
 {
+    [TestFixture]
     public class OrderServiceTests
     {
+        private OrderService _orderService;
+        private Order _order;
+
+        [SetUp]
+        public void Init()
+        {
+            _orderService = new OrderService();   
+        }
+
+        [Test]
+        public void ValidOrder_Returns_OrderSummary()
+        {
+            //Arrange
+            _order = new Order();
+
+            //Act
+            var placedOrder = _orderService.PlaceOrder(_order);
+
+            //Assert
+            Assert.IsInstanceOf<OrderSummary>(placedOrder);
+        }
     }
 }
