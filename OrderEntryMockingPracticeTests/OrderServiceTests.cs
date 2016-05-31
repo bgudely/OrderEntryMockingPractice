43ae1ceac7edf20a404d36401d49ba249beff4cb
Brian Gudely <bgudely@paraport.com>
2016-05-31 12:50:20 -0700

    Multiple exception test is failing

--- OrderEntryMockingPracticeTests/OrderServiceTests.cs
+++ OrderEntryMockingPracticeTests/OrderServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using NSubstitute;
+using NSubstitute.Core;
 using NUnit.Framework;
 using OrderEntryMockingPractice.Models;
 using OrderEntryMockingPractice.Services;
@@ -26,7 +27,16 @@ namespace OrderEntryMockingPracticeTests
         public void ValidOrder_Returns_OrderSummary()
         {
             //Arrange
-            _order = new Order();
+            var product = new Product()
+            {
+                Name = "Test Product",
+                Description = "A test Product",
+                Price = 10,
+                ProductId = 1,
+                Sku = "TestSKU"
+            };
+            _order = GenerateOneProductOrder(product);
+            _productRepository.IsInStock(product.Sku).Returns(true);
 
             //Act
             var placedOrder = _orderService.PlaceOrder(_order);
@@ -49,17 +59,28 @@ namespace OrderEntryMockingPracticeTests
         public void ProductOutOfStock_ThrowsException()
         {
             //Arrange
-            var product = new Product()
-            {
-                Sku = "OutOfStock"
-            };
+            var product = new Product(){ Sku = "OutOfStock" };
             var order = GenerateOneProductOrder(product);
-
-            //Act
             _productRepository.IsInStock(product.Sku).Returns(false);
 
-            //Assert
+            //Act and Assert
             Assert.Throws<InvalidOperationException>(() => _orderService.PlaceOrder(order));
+        }
+
+        [Test]
+        public void DuplicateSkuAndOutOfStock_Returns_AllValidationReasons()
+        {
+            //Arrange
+            var order = GenerateDuplicateSkuOrder();
+            var product = new Product(){ Sku = "OutOfStock" };
+            var orderItem = new OrderItem() { Product = product, Quantity = 1 };
+            order.OrderItems.Add(orderItem);
+
+            _productRepository.IsInStock(product.Sku).Returns(false);
+
+            //Act and Assert
+            var ex = Assert.Throws<InvalidOperationException>(() => _orderService.PlaceOrder(order));
+            Assert.That(ex.Message, Is.EqualTo("SKUs are not unique, A product is out of stock"));
         }
 
         private static Order GenerateOneProductOrder(Product product)
